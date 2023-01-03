ace8070be2bbd54a88ff3b588c6bb1931131f030
Qu1etboy <nonzagreanthai@gmail.com>
2023-01-03 23:11:34 +0700

    feat(css): support dark theme

---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+
+export default function ThemeToggleButton() {
+  const [isMounted, setIsMouted] = useState(false)
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined
+    }
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      return localStorage.getItem('theme')
+    }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark'
+    }
+    return 'light'
+  })
+
+  useEffect(() => {
+    setIsMouted(true)
+  }, [])
+
+  const toggleTheme = () => {
+    const t = theme === 'dark' ? 'light' : 'dark'
+    localStorage.setItem('theme', t)
+    setTheme(t)
+  }
+
+  useEffect(() => {
+    const root = document.documentElement
+    if (theme === 'dark') {
+      root.classList.add('dark')
+    } else {
+      root.classList.remove('dark')
+    }
+  }, [theme])
+
+  return (
+    isMounted && (
+      <button
+        aria-label="Theme Dark Mode"
+        type="button"
+        className="m-3 p-3 text-xl text-white dark:text-neutral-900 rounded-md bg-blue-500 dark:bg-yellow-500 duration-300 ease-in-out hover:bg-blue-700 dark:hover:bg-yellow-600"
+        onClick={toggleTheme}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          className="w-5 h-5 text-gray-200 dark:text-gray-800 fill-gray-200 dark:fill-transparent"
+        >
+          {theme === 'dark' ? (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          ) : (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          )}
+        </svg>
+      </button>
+    )
+  )
+}
